b498a740f4b2730321bcc419dda6019ca3ce43d4
codekid69 <bishtyash069@gmail.com>
2023-06-09 13:44:49 +0530

    changes in api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express');
-// const port=process.env.PORT || 5000;
-const port= 5000;
+const port=process.env.PORT || 5000;
+// const port= 5000;
 const app=express();
 const cors=require('cors');
 const db=require('./config/mongoose');
